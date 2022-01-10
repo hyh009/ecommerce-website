c8e67f4c5e7856037fc4b7798cda0b1b9c95e788
Hsinyao <hsinyao009@gmail.com>
2022-01-10 18:50:06 +0800

    fix import

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@ module.exports = {
   productRoute: require("./product-route"),
   cartRoute: require("./cart-route"),
   orderRoute: require("./order-route"),
-  stripeRoute: require("./stripe-route"),
+  // stripeRoute: require("./stripe-route"),
 };
